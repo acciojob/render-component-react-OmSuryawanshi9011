7ba8f8bf7b8b70b7850e93411571dc26ef84df57
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-02-24 16:22:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+import {useState} from "react";
+
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="root">
+    <p>I am learning React. My life is getting better.</p> </div>
   )
 }
 
